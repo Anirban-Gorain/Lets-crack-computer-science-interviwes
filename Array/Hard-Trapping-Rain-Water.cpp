27a8a1b376859f93d0c58ea9960786c527f6179d
Anirban Gorain <gorainanirban@gmail.com>
2021-06-23 10:04:56 +0530

    Added new sol

--- Array/Hard-Trapping-Rain-Water.cpp
+++ Array/Hard-Trapping-Rain-Water.cpp
@@ -38,6 +38,8 @@ class Solution
 {
     public:
 
+    // T/C = O(n^2), A/S = O(1)
+
     int trap(vector<int>& height)
     {
         // For each _i we will find Left-Max and Right-Max.
@@ -45,6 +47,10 @@ class Solution
 
         int _len = height.size();
         int _rain = 0;
+
+        if(_len==0)
+             return 0;
+         
 
         loop(_i, 1, _len - 1)
         {
@@ -71,6 +77,53 @@ class Solution
 
         return _rain;    
     }
+    
+    // T/C = O(n^2), A/S = O(n)
+
+    int trap_(vector<int>& height)
+    {
+    /* Insted of querying multiple time for l-max & r-max for individual height[_i] we will pre computed
+        the l-max & r-max accordingly.
+        */
+
+    int _n = height.size();
+
+    if(_n==0)
+            return 0;
+        
+
+    int _l_Max[_n];
+    int _r_Max[_n];
+
+    _l_Max[0] = height[0];
+
+    loop(_i, 1, _n)
+    {
+        _l_Max[_i] = max(_l_Max[_i-1], height[_i]);
+    }
+
+    _r_Max[_n-1] = height[_n-1];
+
+    int _j = _n-2;
+
+    while(_j != -1)
+    {
+        _r_Max[_j] = max(_r_Max[_j+1], height[_j]);
+
+        _j--;
+    }
+
+    // Calculating how much water containing each individual i's
+
+    int _rain = 0;
+
+    loop(_k, 1, _n-1)
+    {
+        _rain += (min(_l_Max[_k], _r_Max[_k]) - height[_k]);
+    }
+
+    return _rain;
+    }
 };
 
 int main(void)
@@ -87,7 +140,7 @@ int main(void)
 
     Solution _test;
 
-    cout << _test.trap(_height);
+    cout << _test.trap_(_height);
 
     return 0;
 

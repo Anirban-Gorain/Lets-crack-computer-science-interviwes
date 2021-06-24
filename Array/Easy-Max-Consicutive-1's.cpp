f56def05abc951f5c40dcf571372d7b2f3f2aaf0
Anirban Gorain <gorainanirban@gmail.com>
2021-06-24 10:11:14 +0530

    Solved new question

---
+++ Array/Easy-Max-Consicutive-1's.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+#include<iostream>
+
+using namespace std;
+
+#define f first
+#define s second
+#define ll long long
+#define pub .push_back()
+#define pob .push_back()
+#define mp make_pair
+#define p(_x, y) pair<_x, y>
+#define v(_x) vector<_x>
+#define m(_x, y) map<_x, y>
+#define pq(_x) priority_queue<_x>
+#define setbits(_x) __builtin_popcountll(_x)
+#define zrobits(_x) __builtin_ctzll(_x)
+#define sp(_x, y) fi_xed<<setprecision(y)<<_x
+#define dma(arr,n,type) type *arr=new type[n];
+#define w(_x) int _x; while(_x--)
+#define sq(_x) _x*_x
+#define its(_x) (string) #_x
+#define sti(_x) (int) _x
+#define fti(_x) (int) _x
+#define deb(_x) cout << #_x << " = " << _x << endl;
+#define loop(_x, _s ,_n) for(int _x = _s; _x < _n; ++ _x)
+
+void _fio(void)
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+}
+
+class Solution  
+{
+    public :
+
+    int findMaxConsecutiveOnes(vector<int> & nums)
+    {
+        int _count = 0;
+        int _max = 0;
+        int _n = nums.size();
+
+        loop(_i, 0, _n)
+        {
+            if(nums[_i] == 1)
+            {
+                _count++;
+                _max = max(_count, _max);
+            }
+            else if(nums[_i] == 0)
+            {
+                _count = 0;
+            }
+        }
+
+        return _max;
+    }
+};
+
+int main(void)
+{
+
+    _fio();
+
+    #ifndef ONLINE_JUDGE
+        freopen("C:/Users/Anirban Gorain/Desktop/Competitive-programming/input.txt", "r", stdin);
+        freopen("C:/Users/Anirban Gorain/Desktop/Competitive-programming/output.txt", "w", stdout);
+    #endif
+
+    v(int) _data = {0};
+
+    Solution _test;
+
+    cout << _test.findMaxConsecutiveOnes(_data);
+
+    return 0;
+
+}

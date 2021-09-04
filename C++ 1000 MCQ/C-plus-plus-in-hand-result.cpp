9a82bd48e833784f6dab02684ef2e895502d36d5
Anirban Gorain <gorainanirban@gmail.com>
2021-09-04 21:21:30 +0530

    Learned about Memset function

---
+++ C++ 1000 MCQ/C-plus-plus-in-hand-result.cpp
@@ -0,0 +1,24 @@
+/* 
+
+    1) memset(void* str, int ch, size_t n) --
+    
+    This function defiend on "cstring.h" header file.
+
+    Unsigned-Char : Range [0, 255], Supports extendent ASCII. For overflow, If the overflow is > 0 then will move clockwise. If overflow is < 0 then will move anti clockwise.
+    
+    size_t : This is return type of sizeof() operator. It can store the size of biggest primitive or derived object in a particular OS.
+
+    2) int _arr[10] = {alpha};
+
+    This above method will initialize first block by "alpha" rest of the block automatically will be initialize by zero. That's why int _arr[10] = {0}; works.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+   
+    return 0;
+}

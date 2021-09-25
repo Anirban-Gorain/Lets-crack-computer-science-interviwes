ac8961d2e42a12a41343c7cb7fd7275b24aea184
Anirban Gorain <gorainanirban@gmail.com>
2021-09-25 07:32:14 +0530

    Lrarned about reversing double linked list

---
+++ Linked-List/Medium-Reverse-Doubly-Linked-List.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+#include<iostream>
+
+using namespace std;
+
+#define f first
+#define s second
+#define ll long long
+#define pub .push_back()
+#define pob .push_back()
+#define mp make_pair
+#define p(_x, y) pair<_x, y>
+#define v(_x) vector<_x>
+#define m(_x, y) map<_x, y>
+#define pq(_x) priority_queue<_x>
+#define setbits(_x) __builtin_popcountll(_x)
+#define zrobits(_x) __builtin_ctzll(_x)
+#define sp(_x, y) fi_xed<<setprecision(y)<<_x
+#define dma(arr,n,type) type *arr=new type[n];
+#define w(_x) int _x; while(_x--)
+#define sq(_x) _x*_x
+#define its(_x) (string) #_x
+#define sti(_x) (int) _x
+#define fti(_x) (int) _x
+#define deb(_x) cout << #_x << " = " << _x << endl;
+#define loop(_x, _s ,_n) for(int _x = _s; _x < _n; ++ _x)
+
+void _fio(void)
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+}
+
+
+
+int main(void)
+{
+
+    _fio();
+
+    #ifndef ONLINE_JUDGE
+        freopen("C:/Users/Anirban Gorain/Desktop/Competitive-programming/input.txt", "r", stdin);
+        freopen("C:/Users/Anirban Gorain/Desktop/Competitive-programming/output.txt", "w", stdout);
+    #endif
+
+    _node * _head=new _node(5);
+    _node * _temp1=new _node(6);
+    _node * _temp2=new _node(7);
+
+    _head->_nxt=_temp1;
+    _temp1->_pre=_head;
+
+    _temp1->_nxt=_temp2;
+    _temp2->_pre=_temp1;
+
+    _head=_reverse_DLL(_head);
+
+    for(_node *_it=_head; _it != NULL; _it=_it->_nxt)
+        cout << _it->_data << " ";
+
+    return 0;
+
+}
